3cf2ad8b4332f168016f267f618fe6e872f779c3
Evan1115 <jing111597@gmail.com>
2023-04-14 00:17:47 +0800

    apply font

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Item } from "@/models/items";
 import Items from "../containers/Items/items";
 import styles from "../styles/global.module.css";
+import { codecProUltra, codecproRegular } from "@/utils/font";
 
 const fetchProducts = async () => {
   const data = await fetch(
@@ -9,21 +10,19 @@ const fetchProducts = async () => {
 
   const res = await data.json();
   const items: { items: Item[] } = res;
-  
 
   return items;
 };
 
-
 export default async function Home() {
-
   const { items } = await fetchProducts();
   const itemPerPage = 16;
 
   return (
-    <main className={styles.main_container}>
-      {/* @ts-expect-error Server Component */}
-      <Items items={items} itemPerPage={itemPerPage}/>
+    <main
+      className={`${styles.main_container} + ${codecproRegular.variable} + ${codecProUltra.variable}`}
+    >
+      <Items items={items} itemPerPage={itemPerPage} />
     </main>
   );
 }
