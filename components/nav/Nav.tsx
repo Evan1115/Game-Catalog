0dd258ba0b03c265b9e6eb2b66be516413103f0d
Evan1115 <jing111597@gmail.com>
2023-04-14 00:16:17 +0800

    create second nav bar and add styling

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -3,33 +3,56 @@ import styles from "../../styles/nav.module.css";
 
 function Nav() {
   return (
-    <div className={styles.nav_container}>
-      <nav className={styles.navbar}>
-        <div className={styles.navbar_container}>
-          <div className={styles.navbar_brand}>
-            <a href="#" className={styles.navbar_logo}></a>
+    <div>
+      <div className={styles.navbar}>
+        <div className={styles.container}>
+          <div className={styles.navbar}>
+            <div className={styles.navbar_container}>
+              <div className={styles.navbar_brand}>
+                <Link href="#" className={styles.navbar_logo} />
+              </div>
+              <div className={styles.navbar_menu}>
+                <ul className={styles.navbar_list}>
+                  <li className={styles.avbar_item}>
+                    <div className={styles.navbar_search}>
+                      <input
+                        type="text"
+                        name="search"
+                        placeholder="Search"
+                        className={styles.search_input}
+                      />
+                    </div>
+                  </li>
+                  <li className={styles.navbar_item}>
+                    <Link href="#" className={styles.navbar_link} />
+                    Login/Register
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className={styles.navbar_menu}>
-            <ul className={styles.navbar_list}>
-              <li className={styles.avbar_item}>
-                <div className={styles.navbar_search}>
-                  <input
-                    type="text"
-                    name="search"
-                    placeholder="Search"
-                    className={styles.search_input}
-                  />
-                </div>
+        </div>
+      </div>
+
+      <div className={styles.navbar2}>
+        <div className={styles.container}>
+          <div className={styles.navbar2__bar}>
+            <ul>
+              <li>
+                <Link className={styles.navbar2__link} href="#">
+                  TEAM17 HOME
+                </Link>
               </li>
-              <li className={styles.navbar_item}>
-                <a href="#" className={styles.navbar_link}>
-                  Login/Register
-                </a>
+              <li>
+                <span> &gt; </span>
+              </li>
+              <li>
+                <Link href="#" className={styles.navbar2__link}>STORE</Link>
               </li>
             </ul>
           </div>
         </div>
-      </nav>
+      </div>
     </div>
   );
 }
